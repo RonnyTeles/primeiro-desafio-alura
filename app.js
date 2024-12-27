e12ed4e0665256cf4079a2b4a17096cf2debd41b
RonnyTeles <ronnyrufino@gmail.com>
2024-12-27 01:24:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+alert ('boas vindas ao nosso site');
+
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = 'Erro! preencha todos os campos';
+
+alert (mensagemDeErro);
+
+let nomeN = prompt('Digite seu nome');
+let idadeI = prompt ('Digite sua idade');
+
+if (nome == nomeN);
+if (idade == idadeI);
+if (idade >= 18){
+    alert ('pode tirar habilitacao!');
+};
